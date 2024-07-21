890188ac413931e9d3d9cb42d5d94ed2726caa3d
MAHESH2056 <maheschy2323@gmail.com>
2024-07-21 18:03:28 +0545

    Added Question Page

---
+++ scripts/question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which type of data can be stored in the database?",
+        answer: "All of the above",
+        options: [
+            "Image oriented data",
+            "Text, files containing data",
+            "Data in the form of audio or video",
+            "All of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which command is used to remove a relation from an SQL?",
+        answer: "Drop table",
+        options: [
+            "Drop table",
+            "Delete",
+            "Purge",
+            "Remove"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Procedural language among the following is __________",
+        answer: "Relational algebra",
+        options: [
+            "Domain relational calculus",
+            "Tuple relational calculus",
+            "Relational algebra",
+            "Query language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "CPU scheduling is the basis of ___________",
+        answer: "multiprogramming operating systems",
+        options: [
+            "multiprogramming operating systems",
+            "larger memory sized systems",
+            "multiprocessor systems",
+            "none of the mentioned"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which one of the following is not a real time operating system?",
+        answer: "Palm OS",
+        options: [
+            "RTLinux",
+            "Palm OS",
+            "QNX",
+            "VxWorks"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does OS X has?",
+        answer: "hybrid kernel",
+        options: [
+            "monolithic kernel with modules",
+            "microkernel",
+            "monolithic kernel",
+            "hybrid kernel"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Who invented OOP?",
+        answer: "Alan Kay",
+        options: [
+            "Andrea Ferro",
+            "Adele Goldberg",
+            "Alan Kay",
+            "Dennis Ritchie"
+        ]
+    },
+    {
+        numb: 8,
+        question: "When OOP concept did first came into picture?",
+        answer: "1970’s",
+        options: [
+            "1980’s",
+            "1995",
+            "1970’s",
+            "1993"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which feature of OOP indicates code reusability?",
+        answer: "Inheritance",
+        options: [
+            "Abstraction",
+            "Polymorphism",
+            "Encapsulation",
+            "Inheritance"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which header file is required in C++ to use OOP?",
+        answer: "OOP can be used without using any header file",
+        options: [
+            "OOP can be used without using any header file",
+            "stdlib.h",
+            "iostream.h",
+            "stdio.h"
+        ]
+    },
+];
